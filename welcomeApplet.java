e18a5caf90e0adda08e42af94e8e6c7a997d7b7f
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-30 17:31:57 +0530

    Create welcomeApplet.java

---
+++ welcomeApplet.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+class app extends Applet
+  {
+    public void paint(Graphics g)
+    {
+g.drawString("Welcome",150,150);
+      
+      
+    }
+
+    
+  }
