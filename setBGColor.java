2e41473562717f0f1121ec30ba2fb1f731b093d7
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-30 17:40:02 +0530

    Create setBGColor.java

---
+++ setBGColor.java
@@ -0,0 +1,15 @@
+class app extends Applet
+  {
+public void init()
+    {
+      setBackground(color.YELLOW);
+      resize(500,500);
+    }
+    public void paint(Graphics g)
+
+    {
+g.drawString("Agni Charan Teja",350,350);
+      
+      
+    }
+  }
